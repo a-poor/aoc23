748a81d633936f2b01d12fec2b7018430115087c
Austin Poor <45295232+a-poor@users.noreply.github.com>
2023-12-04 13:34:33 -0800

    Implements day 4 parts 1 and 2

---
+++ src/bin/day04-part1.rs
@@ -0,0 +1,95 @@
+use regex::Regex;
+use anyhow::{anyhow, Result, Context};
+use std::collections::HashMap;
+use aoc23::load_input_lines;
+
+#[allow(dead_code)]
+#[derive(Debug, Default)]
+struct Card {
+    id: usize,
+    winning_numbers: HashMap<usize, usize>,
+    my_numbers: HashMap<usize, usize>,
+}
+
+impl Card {
+    fn parse(line: &str) -> Result<Self> {
+        // Build a regex to parse the line...
+        let re = Regex::new(r"^Card\s+(\d+): ([0-9 ]+) \| ([0-9 ]+)$").unwrap();
+        let caps = re.captures(line).ok_or(anyhow!("Invalid line"))?;
+        
+        // Parse the id, winners, and my nums...
+        let id = caps
+            .get(1)
+            .ok_or(anyhow!("Invalid line - capture group 1 not found"))?
+            .as_str()
+            .parse::<usize>()?;
+        let winning_numbers = caps
+            .get(2)
+            .ok_or(anyhow!("Invalid line - capture group 2 not found"))?
+            .as_str()
+            .split_whitespace()
+            .map(|s| s.parse::<usize>().map_err(anyhow::Error::msg))
+            .collect::<Result<Vec<_>>>()?
+            .into_iter()
+            .fold(HashMap::<usize, usize>::new(), |mut acc, n| {
+                let prev = acc
+                    .get(&n)
+                    .unwrap_or(&0);
+                acc.insert(n, *prev + 1);
+                acc
+            })
+            ;
+        let my_numbers = caps
+            .get(3)
+            .ok_or(anyhow!("Invalid line - capture group 3 not found"))?
+            .as_str()
+            .split_whitespace()
+            .map(|s| s.parse::<usize>().map_err(anyhow::Error::msg))
+            .collect::<Result<Vec<_>>>()?
+            .into_iter()
+            .fold(HashMap::<usize, usize>::new(), |mut acc, n| {
+                let prev = acc
+                    .get(&n)
+                    .unwrap_or(&0);
+                acc.insert(n, *prev + 1);
+                acc
+            })
+            ;
+        
+        // Return the new card...
+        Ok(Self {
+            id,
+            winning_numbers,
+            my_numbers,
+        })
+    }
+
+    fn score(&self) -> usize {
+        let count = self.winning_numbers
+            .iter()
+            .map(|(k, _)| self.my_numbers.contains_key(k))
+            .fold(0, |acc, b| if b { acc + 1 } else { acc })
+            ;
+        if count == 0 { 0 } else { 2_usize.pow(count-1) }
+    }
+}
+
+fn main() -> Result<()> {
+    // Load the input data...
+    let input_lines = load_input_lines(4)?;
+
+    let res = input_lines
+        .into_iter()
+        .enumerate()
+        .map(|(i, line)| {
+            Card::parse(&line)
+                .context(format!("Error parsing line {}", i))
+        })
+        .collect::<Result<Vec<_>>>()?
+        .into_iter()
+        .map(|card| card.score())
+        .sum::<usize>();
+    println!("Done. Result: {}", res);
+
+    Ok(())
+}
---
+++ src/bin/day04-part2.rs
@@ -0,0 +1,92 @@
+use anyhow::{anyhow, Context, Result};
+use aoc23::load_input_lines;
+use regex::Regex;
+use std::collections::HashMap;
+
+#[allow(dead_code)]
+#[derive(Debug, Default, Clone)]
+struct Card {
+    id: usize,
+    win_count: usize,
+}
+
+impl Card {
+    fn parse(line: &str) -> Result<Self> {
+        // Build a regex to parse the line...
+        let re = Regex::new(r"^Card\s+(\d+): ([0-9 ]+) \| ([0-9 ]+)$").unwrap();
+        let caps = re.captures(line).ok_or(anyhow!("Invalid line"))?;
+
+        // Parse the id, winners, and my nums...
+        let id = caps
+            .get(1)
+            .ok_or(anyhow!("Invalid line - capture group 1 not found"))?
+            .as_str()
+            .parse::<usize>()?;
+        let winning_numbers = caps
+            .get(2)
+            .ok_or(anyhow!("Invalid line - capture group 2 not found"))?
+            .as_str()
+            .split_whitespace()
+            .map(|s| s.parse::<usize>().map_err(anyhow::Error::msg))
+            .collect::<Result<Vec<_>>>()?
+            .into_iter()
+            .fold(HashMap::<usize, usize>::new(), |mut acc, n| {
+                let prev = acc.get(&n).unwrap_or(&0);
+                acc.insert(n, *prev + 1);
+                acc
+            });
+        let my_numbers = caps
+            .get(3)
+            .ok_or(anyhow!("Invalid line - capture group 3 not found"))?
+            .as_str()
+            .split_whitespace()
+            .map(|s| s.parse::<usize>().map_err(anyhow::Error::msg))
+            .collect::<Result<Vec<_>>>()?
+            .into_iter()
+            .fold(HashMap::<usize, usize>::new(), |mut acc, n| {
+                let prev = acc.get(&n).unwrap_or(&0);
+                acc.insert(n, *prev + 1);
+                acc
+            });
+
+        let win_count = winning_numbers
+            .iter()
+            .map(|(k, _)| my_numbers.contains_key(k))
+            .fold(0, |acc, b| if b { acc + 1 } else { acc });
+
+        // Return the new card...
+        Ok(Self { id, win_count })
+    }
+}
+
+fn main() -> Result<()> {
+    // Load the input data...
+    let input_lines = load_input_lines(4)?;
+
+    // Parse the input data...
+    let cards = input_lines
+        .into_iter()
+        .enumerate()
+        .map(|(i, line)| Card::parse(&line).context(format!("Error parsing line {}", i)))
+        .collect::<Result<Vec<_>>>()?;
+
+    // Create a queue of cards...
+    let mut counts = cards.iter().map(|_| 1_usize).collect::<Vec<_>>();
+
+    // Now start looping through the cards...
+    for (i, c) in cards.into_iter().enumerate() {
+        // Now, for each n coppies of the i-th card,
+        // add n copies to the next m cards, where m
+        // is the number of winners for this card...
+        for j in 1..=c.win_count {
+            counts[i + j] += counts[i];
+        }
+    }
+
+    // Count the total number of cards...
+    let res = counts.into_iter().sum::<usize>();
+
+    // Print out the result...
+    println!("Done. Result: {}", res);
+    Ok(())
+}
