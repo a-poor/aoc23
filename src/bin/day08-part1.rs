ad384b0309674c97d800389c4feec018f68d5670
Austin Poor <45295232+a-poor@users.noreply.github.com>
2023-12-08 06:55:27 -0800

    Implement day 8 part 1

--- src/bin/day08-part1.rs
+++ src/bin/day08-part1.rs
@@ -1,23 +1,23 @@
-use std::collections::HashMap;
-use regex::Regex;
 use anyhow::{anyhow, Result};
 use aoc23::load_input_lines_by_name;
+use regex::Regex;
+use std::collections::HashMap;
 
 fn parse_line(line: &str) -> Result<(String, String, String)> {
-    let re = Regex::new(r"([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)")
-        .unwrap();
-    let caps = re
-        .captures(line)
-        .ok_or(anyhow!("Invalid line: {}", line))?;
-    let id = caps.get(1)
+    let re = Regex::new(r"([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)").unwrap();
+    let caps = re.captures(line).ok_or(anyhow!("Invalid line: {}", line))?;
+    let id = caps
+        .get(1)
         .ok_or(anyhow!("Regex didn't match against line \"{}\"", line))?
         .as_str()
         .to_string();
-    let left = caps.get(2)
+    let left = caps
+        .get(2)
         .ok_or(anyhow!("Regex didn't match against line \"{}\"", line))?
         .as_str()
         .to_string();
-    let right = caps.get(3)
+    let right = caps
+        .get(3)
         .ok_or(anyhow!("Regex didn't match against line \"{}\"", line))?
         .as_str()
         .to_string();
@@ -25,7 +25,7 @@ fn parse_line(line: &str) -> Result<(String, String, String)> {
 }
 
 fn main() -> Result<()> {
-    // let lines = load_input_lines_by_name(file!())?;
+    let lines = load_input_lines_by_name(file!())?;
     // let lines = vec![
     //     "RL".to_string(),
     //     "".to_string(),
@@ -37,31 +37,32 @@ fn main() -> Result<()> {
     //     "GGG = (GGG, GGG)".to_string(),
     //     "ZZZ = (ZZZ, ZZZ)".to_string(),
     // ];
-    let lines = vec![
-        "LLR".to_string(),
-        "".to_string(),
-        "AAA = (BBB, BBB)".to_string(),
-        "BBB = (AAA, ZZZ)".to_string(),
-        "ZZZ = (ZZZ, ZZZ)".to_string(),
-    ];
+    // let lines = vec![
+    //     "LLR".to_string(),
+    //     "".to_string(),
+    //     "AAA = (BBB, BBB)".to_string(),
+    //     "BBB = (AAA, ZZZ)".to_string(),
+    //     "ZZZ = (ZZZ, ZZZ)".to_string(),
+    // ];
     let directions = lines.get(0).ok_or(anyhow!("No input"))?;
     let nodes = lines[2..]
         .into_iter()
         .map(|l| parse_line(l))
         .collect::<Result<Vec<_>>>()?
         .into_iter()
-        .fold(HashMap::<String,(String,String)>::new(), |mut acc, (id, left, right)| {
-            acc.insert(id, (left, right));
-            acc
-        });
+        .fold(
+            HashMap::<String, (String, String)>::new(),
+            |mut acc, (id, left, right)| {
+                acc.insert(id, (left, right));
+                acc
+            },
+        );
 
     let mut node = "AAA".to_string();
+    let mut i: usize = 0;
     let mut count: usize = 0;
     while node != "ZZZ" {
-        let (left, right) = nodes
-            .get(&node)
-            .ok_or(anyhow!("No node {}", node))?;
-        let i = count % 2;
+        let (left, right) = nodes.get(&node).ok_or(anyhow!("No node {}", node))?;
         let direction = directions
             .chars()
             .nth(i)
@@ -72,10 +73,10 @@ fn main() -> Result<()> {
             _ => return Err(anyhow!("Invalid direction {}", direction)),
         };
         count += 1;
+        i = if i == directions.len() - 1 { 0 } else { i + 1 };
     }
 
     println!("count: {}", count);
 
     Ok(())
 }
-
