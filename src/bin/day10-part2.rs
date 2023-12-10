1e3f081ae662cd3fd603b6dde26b6e7a2d3aa193
Austin Poor <45295232+a-poor@users.noreply.github.com>
2023-12-10 10:06:14 -0800

    Implements day 10 part 2

--- src/bin/day10-part2.rs
+++ src/bin/day10-part2.rs
@@ -1,8 +1,8 @@
-use std::collections::HashSet;
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use aoc23::load_input_lines_by_name;
+use std::collections::HashSet;
 
-#[derive(Debug,Hash,Clone,Copy,PartialEq,Eq)]
+#[derive(Debug, Hash, Clone, Copy, PartialEq, Eq)]
 struct Point {
     x: i32,
     y: i32,
@@ -12,28 +12,25 @@ impl Point {
     fn new(x: i32, y: i32) -> Self {
         Self { x, y }
     }
-    
+
     fn new_us(x: usize, y: usize) -> Result<Self> {
-        Ok(Self::new(
-            x.try_into()?, 
-            y.try_into()?,
-        ))
+        Ok(Self::new(x.try_into()?, y.try_into()?))
     }
 
     fn north(&self) -> Self {
-        Self::new(self.x, self.y-1)
+        Self::new(self.x, self.y - 1)
     }
 
     fn south(&self) -> Self {
-        Self::new(self.x, self.y+1)
+        Self::new(self.x, self.y + 1)
     }
 
     fn east(&self) -> Self {
-        Self::new(self.x+1, self.y)
+        Self::new(self.x + 1, self.y)
     }
 
     fn west(&self) -> Self {
-        Self::new(self.x-1, self.y)
+        Self::new(self.x - 1, self.y)
     }
 }
 
@@ -43,7 +40,7 @@ impl std::fmt::Display for Point {
     }
 }
 
-#[derive(Debug,Clone,Copy,PartialEq,Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Pipe {
     Start,
     Ground,
@@ -68,7 +65,7 @@ impl TryFrom<char> for Pipe {
             'J' => Ok(Pipe::NorthWest),
             'F' => Ok(Pipe::SouthEast),
             '7' => Ok(Pipe::SouthWest),
-            _ => Err(anyhow!("Invalid pipe character: {}", c))
+            _ => Err(anyhow!("Invalid pipe character: {}", c)),
         }
     }
 }
@@ -148,33 +145,54 @@ fn get_connections(grid: &Vec<Vec<Pipe>>, point: &Point) -> Option<(Point, Point
                 return Some((south, west));
             }
             panic!("Start point has no connections");
-        },
-        Pipe::NorthSouth => Some((
-            point.north(),
-            point.south(),
-        )),
-        Pipe::EastWest => Some((
-            point.east(),
-            point.west(),
-        )),
-        Pipe::NorthEast => Some((
-            point.north(),
-            point.east(),
-         )),
-        Pipe::NorthWest => Some((
-            point.north(),
-            point.west(),
-        )),
-        Pipe::SouthEast => Some((
-            point.south(),
-            point.east(),
-        )),
-        Pipe::SouthWest => Some((
-            point.south(),
-            point.west(),
-        )),
+        }
+        Pipe::NorthSouth => Some((point.north(), point.south())),
+        Pipe::EastWest => Some((point.east(), point.west())),
+        Pipe::NorthEast => Some((point.north(), point.east())),
+        Pipe::NorthWest => Some((point.north(), point.west())),
+        Pipe::SouthEast => Some((point.south(), point.east())),
+        Pipe::SouthWest => Some((point.south(), point.west())),
         Pipe::Ground => None,
     }
+}
+
+/// Infers the pipe type at `point`, given it's neighbors.
+fn infer(grid: &Vec<Vec<Pipe>>, point: &Point) -> Result<Pipe> {
+    let (a, b) = get_connections(grid, point)
+        .ok_or(anyhow!("Unable to find connections to point {}", point))?;
+
+    // Get the neighbors...
+    let north = point.north();
+    let south = point.south();
+    let east = point.east();
+    let west = point.west();
+
+    // Check which sides it connects to...
+    let connects_north = a == north || b == north;
+    let connects_south = a == south || b == south;
+    let connects_east = a == east || b == east;
+    let connects_west = a == west || b == west;
+
+    // Check the combos...
+    if connects_north && connects_south {
+        return Ok(Pipe::NorthSouth);
+    }
+    if connects_north && connects_east {
+        return Ok(Pipe::NorthEast);
+    }
+    if connects_north && connects_west {
+        return Ok(Pipe::NorthWest);
+    }
+    if connects_south && connects_east {
+        return Ok(Pipe::SouthEast);
+    }
+    if connects_south && connects_west {
+        return Ok(Pipe::SouthWest);
+    }
+    if connects_east && connects_west {
+        return Ok(Pipe::EastWest);
+    }
+    Err(anyhow!("Unable to infer pipe type at {}", point))
 }
 
 fn main() -> Result<()> {
@@ -201,13 +219,21 @@ fn main() -> Result<()> {
                 .collect::<Result<Vec<Pipe>>>()
         })
         .collect::<Result<Vec<Vec<Pipe>>>>()?;
- 
+
     // Find the starting point...
     let start = find_start(&grid)?;
-    
+
+    // Replace the start pipe with its inferred type...
+    let grid = {
+        let start_pipe = infer(&grid, &start).context("Failed to infer start pipe type")?;
+        let mut grid = grid;
+        let row = &mut grid[start.y as usize];
+        row[start.x as usize] = start_pipe;
+        grid
+    };
+
     // Find the connected points...
-    let (next, _) = get_connections(&grid, &start)
-        .ok_or(anyhow!("No connections found"))?;
+    let (next, _) = get_connections(&grid, &start).ok_or(anyhow!("No connections found"))?;
 
     // Follow the two paths back to the start...
     let path_points: HashSet<Point> = {
@@ -215,8 +241,7 @@ fn main() -> Result<()> {
         let mut prev = start;
         let mut this = next;
         while this != start {
-            let (a, b) = get_connections(&grid, &this)
-                .ok_or(anyhow!("No connections found"))?;
+            let (a, b) = get_connections(&grid, &this).ok_or(anyhow!("No connections found"))?;
             if a != prev {
                 path.push(a);
                 prev = this;
@@ -227,87 +252,150 @@ fn main() -> Result<()> {
                 this = b;
             }
         }
-        path
-            .iter()
-            .copied()
-            .collect()
+        path.iter().copied().collect()
     };
 
-    // Find the points that are not contained 
-    // by the path. Start by seeding the in/out
-    // sets with points on the edge of the map
-    // and points on the line...
+    // Create sets to track points inside/outside
+    // the path...
     let mut points_out = HashSet::<Point>::new();
-    // let mut points_in = HashSet::<Point>::new();
-    let mut points_unsure = HashSet::<Point>::new();
+    let mut points_in = HashSet::<Point>::new();
+    // let mut points_unsure = HashSet::<Point>::new();
+
+    // Iterate through the grid...
+    //
+    // For each row, imagine drawing a line from left
+    // to right, starting off the left side, and ending
+    // off the right side.
+    //
+    // As we start, we know we're outside the path and
+    // any point we encounter is outside the path. But
+    // if we cross the path, then everything flips and
+    // any point we encounter is inside the path --
+    // until we cross the path again.
+    //
+    // For example, the following cross section shows
+    // how the markers would flip, moving left to right:
+    //
+    // ```
+    // O O   I I   O
+    // ↓ ↓   ↓ ↓   ↓
+    // . . | . . | .
+    // ```
+    //
+    // But...we need to account for cases where we touch
+    // the path but don't *cross* it. For example:
+    //
+    // ```
+    // O       O   I
+    // ↓       ↓   ↓
+    // . L - J . | .
+    // ```
+    //
+    // If we flipped the marker at each path point, we'd
+    // end up with `OIO` instead of `OOI`. Also consider:
+    //
+    // ```
+    // O       I   O
+    // ↓       ↓   ↓
+    // . L - 7 . | .
+    // ```
+    //
+    // I think the key here is that with the `|` pipe,
+    // we're fully crossing the path, so we flip the
+    // marker.
+    //
+    // With the `-` pipe, we're following the path, so
+    // we don't do anything.
+    //
+    // With the `LJ7F` pipes, we're only *half* crossing
+    // the path. Depending on the *state* that we're in.
+    // If we're fully in or out, the only (valid) half
+    // steps we should see are `L` or `F`. If we see one
+    // of those, we don't flip the marker yet. Instead
+    // track the half-step. From then on, we should see
+    // zero or more `-` pipes until we reach the next
+    // half-step -- either `J` or `7`. Then, if the two
+    // markers *combine* to form a `|` pipe, we flip the
+    // marker, otherwise we don't.
+    //
+    // ```
+    // F + J = |
+    // L + 7 = |
+    // ```
+    //
     for (y, row) in grid.iter().enumerate() {
-        for (x, _) in row.iter().enumerate() {
+        // Initialize the marker and the half-state...
+        let mut mark_in = false;
+        let mut half_state: Option<Pipe> = None;
+        for (x, pipe) in row.iter().enumerate() {
             // Construct the point...
             let point = Point::new_us(x, y)?;
 
-            // If the point is in the path, skip it... 
+            // If the point is in the path, check to flip...
             if path_points.contains(&point) {
+                match *pipe {
+                    Pipe::NorthSouth => {
+                        // Flip!
+                        mark_in = !mark_in;
+                    }
+                    Pipe::EastWest => {} // no-op
+                    Pipe::NorthEast => {
+                        // Mark and continue...
+                        half_state = Some(*pipe);
+                    }
+                    Pipe::SouthEast => {
+                        // Mark and continue...
+                        half_state = Some(*pipe);
+                    }
+                    Pipe::NorthWest => {
+                        // Check state and maybe flip...
+                        let from = half_state.ok_or(anyhow!("No half-state found"))?;
+                        match from {
+                            Pipe::SouthEast => {
+                                // Flip!
+                                mark_in = !mark_in;
+                            }
+                            Pipe::NorthEast => {} // no-op
+                            _ => unreachable!("How'd we get here!? Was {:?} is {:?}", from, pipe),
+                        }
+                    }
+                    Pipe::SouthWest => {
+                        // Check state and maybe flip...
+                        let from = half_state.ok_or(anyhow!("No half-state found"))?;
+                        match from {
+                            Pipe::NorthEast => {
+                                // Flip!
+                                mark_in = !mark_in;
+                            }
+                            Pipe::SouthEast => {} // no-op
+                            _ => unreachable!("How'd we get here!? Was {:?} is {:?}", from, pipe),
+                        }
+                    }
+                    _ => unreachable!(
+                        "Ground and start pipes should not be in the path: {:?}",
+                        pipe
+                    ),
+                }
                 continue;
             }
 
-            // If it's on an edge, add it to the
-            // points out...
-            if x == 0 || y == 0 || x == row.len()-1 || y == grid.len()-1 {
+            // Otherwise, add it to the marked bucket...
+            if mark_in {
+                points_in.insert(point);
+            } else {
                 points_out.insert(point);
-                continue;
             }
-            
-            // If we get here, the point is inside the path...
-            points_unsure.insert(point);
-        }
-    }
-    
-    // Now, using that as a starting point,
-    // iterate through the points we're unsure
-    // of, and check their neighbors...
-    // for i in 0..20 {
-    // while points_unsure.len() > 0 {
-    loop {
-        println!("{:6} points unsure", points_unsure.len());
-
-        let queue = points_unsure;
-        points_unsure = HashSet::new();
-        
-        for point in queue.iter() {
-            // Are any of it's neighbors outside the path?
-            // (Note: No neighbors should be on the edge or
-            // within the path -- from previous checks --
-            // so we don't need to check for that.)
-            if points_out.contains(&point.north()) 
-                || points_out.contains(&point.south()) 
-                || points_out.contains(&point.east()) 
-                || points_out.contains(&point.west()) 
-            {
-                points_out.insert(*point);
-                continue;
-            }
-
-            // Otherwise, we're still unsure...
-            points_unsure.insert(*point);
-        }
-
-        // If the number of points is unchanged, we're done...
-        if points_unsure.len() == queue.len() {
-            break;
         }
     }
 
-    // Okay, now to find the points that ar
-    
-    
     // Get the grid size...
     let height = grid.len();
     let width = grid[0].len();
     let n_points_total = width * height;
-
-    // Get the number of points inside the path...
-    let count = n_points_total - points_out.len() - path_points.len();
-    println!("{} points inside the path", count);
+    println!("Points total = {}", n_points_total);
+    println!("Points on path = {}", path_points.len());
+    println!("Points outside path = {}", points_out.len());
+    println!("Points inside path = {}", points_in.len());
 
     Ok(())
 }
